ec177b09b86ab7a4e93680d459624c7f82720d5e
Moath <109114154+MoaathK@users.noreply.github.com>
2023-10-25 00:37:57 +0300

    mdified on lab5

--- Labs/Lab5/ThreadChecker.java
+++ Labs/Lab5/ThreadChecker.java
@@ -10,35 +10,49 @@ public class ThreadChecker implements Runnable{
     public ThreadChecker(File directory){
         this.directory = directory;
 
+
     }
 
     @Override
     public void run() {
 
-        System.out.println("Thread Name: "+Thread.currentThread().getName());
+
+
+        System.out.println("Thread: "+Thread.currentThread().toString());
         File[] files = directory.listFiles();
         if (files != null){
             for (int i = 0; i < files.length ; i++){
+
+
                 if (files[i].isDirectory()){
                     Thread subDirectory = new Thread(new ThreadChecker(files[i]));
                     subDirectory.start();
+                    try {
+                        subDirectory.join();
+                    }catch (InterruptedException e){
+
+                    }
                     printData(files[i]);
                 }
                 else {
                     printData(files[i]);
                 }
+
             }
         }
     }
-    public void printData(File file){
+
+    public void printData(File file) {
+
+
         System.out.println("File Name: "+ file.getName());
-        System.out.println("File Can write: "+file.canWrite() );
-        System.out.println("File can read: "+file.canRead() );
+        System.out.println("File Can write: "+file.canWrite());
+        System.out.println("File can read: "+file.canRead());
         System.out.println("File can execute: "+ file.canExecute());
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date lastModified = new Date(file.lastModified());
         System.out.println("Last Modified: " + dateFormat.format(lastModified));
+        System.out.println("--------------------------");
 
-        System.out.println("-------------------------------------");
     }
 }
