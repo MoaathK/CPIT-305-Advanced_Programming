e0f0919f663c43490e24ded5f6cdd8596093ac7c
Moath <109114154+MoaathK@users.noreply.github.com>
2023-10-29 19:02:57 +0300

    New update on the lab

--- Labs/Lab-6/src/Main.java
+++ Labs/Lab-6/src/Main.java
@@ -1,8 +1,10 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class Main{
@@ -14,11 +16,39 @@ public class Main{
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the File/Directory full path: ");
         File file = new File(input.nextLine());
+        File output = new File("Output.txt");
+        ReentrantLock reentrantLock = new ReentrantLock();
+        RandomAccessFile randomAccessFile = new RandomAccessFile(output, "rw");
         if (file.isDirectory()){
-            Thread root = new Thread(new ThreadChecker(file));
+            Thread root = new Thread(new ThreadChecker(file,reentrantLock));
             root.start();
 
         }
+        //File file1 = new File("data.dat");
+
+        /*try {
+            // Create a RandomAccessFile and a FileChannel for the file
+
+            FileChannel fileChannel = randomAccessFile.getChannel();
+
+            // Acquire an exclusive lock on the file
+            FileLock fileLock = fileChannel.lock();
+
+            // Now, you can safely write objects to the file using ObjectOutputStream
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(randomAccessFile.getFD()));
+            //MyObject obj = new MyObject(); // Replace with your object
+
+            //outputStream.writeObject(obj);
+
+            // Release the lock when done
+            fileLock.release();
+
+            // Close the file and file channel
+            outputStream.close();
+            fileChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
 
 
--- Labs/Lab-6/src/ThreadChecker.java
+++ Labs/Lab-6/src/ThreadChecker.java
@@ -1,15 +1,22 @@
-import java.io.File;
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 public class ThreadChecker implements Runnable{
     private File directory;
+    private ReentrantLock reentrantLock;
+    File
 
-
-    public ThreadChecker(File directory){
+    public ThreadChecker(File directory, ReentrantLock reentrantLock){
         this.directory = directory;
 
+        this.reentrantLock = reentrantLock;
 
     }
 
@@ -25,7 +32,7 @@ public class ThreadChecker implements Runnable{
 
 
                 if (files[i].isDirectory()){
-                    Thread subDirectory = new Thread(new ThreadChecker(files[i]));
+                    Thread subDirectory = new Thread(new ThreadChecker(files[i],reentrantLock));
                     subDirectory.start();
                     try {
                         subDirectory.join();
@@ -43,16 +50,35 @@ public class ThreadChecker implements Runnable{
     }
 
     public void printData(File file) {
+        reentrantLock.lock();
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("Output.txt"))){
+            os.writeObject(file);
 
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+/*
+        try (FileChannel channel1 = FileChannel.open(path, StandardOpenOption.APPEND)){
+            FileLock lock = channel1.tryLock();
+            ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(path.toFile()));
+            outputStream.writeObject(file);
+            lock.release();
+            channel1.close();
+            outputStream.close();
 
-        System.out.println("File Name: "+ file.getName());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+        /*System.out.println("File Name: "+ file.getName());
         System.out.println("File Can write: "+file.canWrite());
         System.out.println("File can read: "+file.canRead());
         System.out.println("File can execute: "+ file.canExecute());
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date lastModified = new Date(file.lastModified());
         System.out.println("Last Modified: " + dateFormat.format(lastModified));
-        System.out.println("--------------------------");
+        System.out.println("--------------------------");*/
 
     }
 }
