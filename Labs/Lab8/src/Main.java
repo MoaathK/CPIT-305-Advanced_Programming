2618179571b23ea17a111b5ddab671ce320092f9
Moath Alahmadi <MoaathK@users.noreply.github.com>
2023-11-19 03:38:40 +0300

    Finished the lab

--- Labs/Lab8/src/Main.java
+++ Labs/Lab8/src/Main.java
@@ -1,15 +1,5 @@
-
-
-
-import netscape.javascript.JSObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
